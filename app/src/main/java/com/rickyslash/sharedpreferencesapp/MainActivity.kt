42dfab269463416f60e64670e19ea82202ccda7e
ricky-kiva <ricky.kiva@gmail.com>
2023-03-27 15:33:30 +0700

    edit MainActivity

--- app/src/main/java/com/rickyslash/sharedpreferencesapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/sharedpreferencesapp/MainActivity.kt
@@ -1,12 +1,89 @@
 package com.rickyslash.sharedpreferencesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.rickyslash.sharedpreferencesapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mUserPreference: UserPreference
+
+    private var isPreferenceEmpty = false
+    private lateinit var userModel: UserModel
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.data != null && result.resultCode == FormUserPreferenceActivity.RESULT_CODE) {
+            userModel = result.data?.getParcelableExtra<UserModel>(FormUserPreferenceActivity.EXTRA_RESULT) as UserModel
+            populateView(userModel)
+            checkForm(userModel)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "My User Preference"
+
+        mUserPreference = UserPreference(this)
+
+        showExistingPreference()
+
+        binding.btnSave.setOnClickListener(this)
+    }
+
+    private fun showExistingPreference() {
+        userModel = mUserPreference.getUser()
+        populateView(userModel)
+        checkForm(userModel)
+    }
+
+    private fun populateView(userModel: UserModel) {
+        binding.tvName.text = if (userModel.name.toString().isEmpty()) "Empty" else userModel.name
+        binding.tvAge.text = if (userModel.age.toString().isEmpty()) "Empty" else userModel.age.toString()
+        binding.tvIsLoveMu.text = if (userModel.isLove) "Yes" else "No"
+        binding.tvEmail.text = if (userModel.email.toString().isEmpty()) "Empty" else userModel.email
+        binding.tvPhone.text = if (userModel.phoneNumber.toString().isEmpty()) "Empty" else userModel.phoneNumber
+    }
+
+    private fun checkForm(userModel: UserModel) {
+        when {
+            userModel.name.toString().isNotEmpty() -> {
+                binding.btnSave.text = getString(R.string.change)
+                isPreferenceEmpty = false
+            }
+            else -> {
+                binding.btnSave.text = getString(R.string.add)
+                isPreferenceEmpty = true
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_save) {
+            val intent = Intent(this@MainActivity, FormUserPreferenceActivity::class.java)
+            when {
+                isPreferenceEmpty -> {
+                    intent.putExtra(FormUserPreferenceActivity.EXTRA_TYPE_FORM, FormUserPreferenceActivity.TYPE_ADD)
+                    intent.putExtra("USER", userModel)
+                }
+                else -> {
+                    intent.putExtra(FormUserPreferenceActivity.EXTRA_TYPE_FORM, FormUserPreferenceActivity.TYPE_EDIT)
+                    intent.putExtra("USER", userModel)
+                }
+            }
+            resultLauncher.launch(intent)
+        }
     }
 }
 
